93c6f8274aacaca776ed04b9700744df6a9e7033
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-05-20 20:11:27 +0300

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.map((galItem) => `<li class="gallery__item">
+<a class="gallery__link" href="${galItem.original}">
+    <img
+        class="gallery__image"
+        src="${galItem.preview}"
+        data-source="${galItem.original}"
+        alt="${galItem.description}"
+    />
+</a>
+</li>`).join("");
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+
 console.log(galleryItems);
+
+gallery.addEventListener("click", popUpImage);
+
+function popUpImage(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    let bigImg = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+    <img src="${bigImg}" width="800" height="600">`)
+
+    instance.show()
+
+}
+
