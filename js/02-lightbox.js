1333bbbdce5400c0f9281c253acc236509b3a98b
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-05-20 23:25:18 +0300

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.map((galItem) => `<li class="gallery__item">
+<a class="gallery__link" href="${galItem.original}">
+    <img
+        class="gallery__image"
+        src="${galItem.preview}"
+        alt="${galItem.description}"
+    />
+</a>
+</li>`).join("");
+gallery.insertAdjacentHTML("afterbegin", markup);
+
 console.log(galleryItems);
+
+let gallerySLbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
